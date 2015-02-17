2adca1100a2e5607cc9e4af8f96a6e40de4f57cf
TDouch <t.douchie@hotmail.com>
2015-02-17 14:24:16 +0000

    Treasure Draw
    needs a function that will redraw treasures

--- drawTreasures.py
+++ drawTreasures.py
@@ -5,38 +5,33 @@ pygame.init()
 size = [1280, 710]
 WHITE = (255,255,255)
 BLUE = (0,0,255)
-test ={'t' : (145, 138), 'a' : (75, 258) ,
+coord ={'t' : (145, 138), 'a' : (75, 258) ,
        'b' : (275, 258), 'e' : (165, 378),
        'g' : (95 , 498), 's' : (295, 498),
        'f' : (395, 378), 'h' : (535, 498),
        'c' : (495, 258), 'j' : (745, 258),
        'i' : (795, 498), 'd' : (695, 378)}
 
-
-landmarks ={'t' : 0, 'a' : 0 ,
-           'b' : 0, 'e' : 0,
-           'g' : 0, 's' : 0,
-           'f' : 0, 'h' : 0,
-           'c' : 0, 'j' : 0,
-           'i' : 0, 'd' : 0}
+landmarks ={'t' : False, 'a' : False,
+           'b' : False, 'e' : False,
+           'g' : False, 's' : False,
+           'f' : False, 'h' : False,
+           'c' : False, 'j' : False,
+           'i' : False, 'd' : False}
 
 def landmark(location):
     landmarks[location] = 1
-
-    
-def drawDiamond(coords):
-    x, y = (test[coords])
+    x, y = (coord[location])
     rectangle = pygame.draw.rect(screen, BLUE,(x,y, 10,  10 ))
-    pygame.display.update()
-    
+
     
 def location():
     treasure1 = raw_input("Input treasure location: ")
-    drawDiamond(treasure1)
     landmark(treasure1)
     
 def locationcheck(location):
     if landmarks[location] == 1:
+        
         print 'There is a treasure at location ' + location + ' gain x amount of points.'
     else:
         print 'There is not a treaure at this location.'
