57aca74d6b1ca0f7ace0768ca58f353fa50f93b1
Matthew Joseph <josephm9@uni.coventry.ac.uk>
2015-02-17 14:59:55 +0000

    Draw all treasures
    new variable names and cleaning

--- drawTreasures.py
+++ drawTreasures.py
@@ -2,49 +2,54 @@ import time
 import pygame
 pygame.init()
 
-size = [1280, 710]
+size = [1280, 720]
 WHITE = (255,255,255)
 BLUE = (0,0,255)
-coord ={'t' : (145, 138), 'a' : (75, 258) ,
-       'b' : (275, 258), 'e' : (165, 378),
-       'g' : (95 , 498), 's' : (295, 498),
-       'f' : (395, 378), 'h' : (535, 498),
-       'c' : (495, 258), 'j' : (745, 258),
-       'i' : (795, 498), 'd' : (695, 378)}
 
-landmarks ={'t' : False, 'a' : False,
-           'b' : False, 'e' : False,
-           'g' : False, 's' : False,
-           'f' : False, 'h' : False,
-           'c' : False, 'j' : False,
-           'i' : False, 'd' : False}
+landmarkCoords ={'t' : (145, 138), 'a' : (75, 258) ,
+                 'b' : (275, 258), 'e' : (165, 378),
+                 'g' : (95 , 498), 's' : (295, 498),
+                 'f' : (395, 378), 'h' : (535, 498),
+                 'c' : (495, 258), 'j' : (745, 258),
+                 'i' : (795, 498), 'd' : (695, 378)}
 
-def landmark(location):
-    landmarks[location] = 1
-    x, y = (coord[location])
-    rectangle = pygame.draw.rect(screen, BLUE,(x,y, 10,  10 ))
+landmarkTreasureState ={'t' : False, 'a' : False,
+                        'b' : False, 'e' : False,
+                        'g' : False, 's' : False,
+                        'f' : False, 'h' : False,
+                        'c' : False, 'j' : False,
+                        'i' : False, 'd' : False}
 
-    
-def location():
-    treasure1 = raw_input("Input treasure location: ")
-    landmark(treasure1)
-    
+
+
+def userInputTreasureLocation():
+    treasureLocation = raw_input("Input treasure location: ")
+    #landmark(treasureLocation)
+    landmarkTreasureState[treasureLocation] = True
+
+def drawAllTreasures():
+    for key in landmarkTreasureState:
+        if landmarkTreasureState[key] == True:
+            x, y = (landmarkCoords[key])
+            pygame.draw.rect(screen, BLUE,(x,y,10,10))
+    pygame.display.flip()
+                 
 def locationcheck(location):
-    if landmarks[location] == 1:
-        
+    if landmarkTreasureState[location] == True :
+            
         print 'There is a treasure at location ' + location + ' gain x amount of points.'
     else:
         print 'There is not a treaure at this location.'
 
 screen = pygame.display.set_mode(size)
-
 screen.fill(WHITE)
-location()
-location()
-location()
-location()
-location()
-location()
+
+userInputTreasureLocation()
+userInputTreasureLocation()
+userInputTreasureLocation()
+userInputTreasureLocation()
+userInputTreasureLocation()
+drawAllTreasures()              
 pygame.event.wait()
 
 locationcheck('a')
