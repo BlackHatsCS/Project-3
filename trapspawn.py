c7efe219d03c01c8dd37874b6b3138f2fa8b5cf1
TheBastardChild <mithellin@hotmail.com>
2015-02-27 00:03:31 +0000

    Update trapspawn.py
    Here is what I believe to be the finished thing, implement it in the session after the lecture I guess?

--- trapspawn.py
+++ trapspawn.py
@@ -1,5 +1,7 @@
 import random
 import time
+import pygame
+
 
 t = 145, 138
 a = 75, 258
@@ -17,10 +19,9 @@ d = 695, 378
 nodes = [t, a, b, e, g, s, f, h, c, j, i, d]
 
 def spawn():
-    global trapspawn
     rspawn = random.randint(0,3)
     tspawn = random.randint(0,3)
-    trapspawn = random.choice(nodes)
+    trspawn = random.randint(0,11)
     global treasure
     if tspawn == 0:
          treasure = 't'
@@ -37,14 +38,55 @@ def spawn():
         rstart = 'j'
     elif rspawn == 2:
         rstart = 'd'
-    elif rspawn ==3:
+    elif rspawn == 3:
         rstart = 'i'
-    if rspawn == 'c':
-        trapspawn != 'c'
-    elif rspawn == 'j':
-        trapspawn != 'j'
-    elif rspawn == 'i':
-        trapspawn != 'i'
-    elif rspawn == 'd':
-        trapspawn != 'd'
+    global trap
+    if trspawn == 0:
+        trap = 't'
+        pygame.draw.rect(screen, RED, [145, 138, 50, 50])
+    elif trspawn == 1:
+        trap = 'a'
+        rstart != 'a'
+        pygame.draw.rect(screen, RED, [75, 258, 50, 50])
+    elif trspawn == 2:
+        trap = 'b'
+        rstart != 'b'
+        pygame.draw.rect(screen, RED, [275, 258, 50, 50])
+    elif trspawn == 3:
+        trap = 'e'
+        rstart != 'e'
+        pygame.draw.rect(screen, RED, [165, 378, 50, 50])
+    elif trspawn == 4:
+        trap = 'g'
+        rstart != 'g'
+        pygame.draw.rect(screen, RED, [95, 498, 50, 50])
+    elif trspawn == 5:
+        trap = 's'
+        rstart != 's'
+        pygame.draw.rect(screen, RED, [295, 498, 50, 50])
+    elif trspawn == 6:
+        trap = 'f'
+        rstart != 'f'
+        pygame.draw.rect(screen, RED, [395, 378, 50, 50])
+    elif trspawn == 7:
+        trap = 'h'
+        rstart != 'h'
+        pygame.draw.rect(screen, RED, [535, 498, 50, 50])
+    elif trspawn == 8:
+        trap = 'c'
+        rstart != 'c'
+        pygame.draw.rect(screen, RED, [495, 258, 50, 50])
+    elif trspawn == 9:
+        trap = 'j'
+        rstart != 'j'
+        pygame.draw.rect(screen, RED, [745, 258, 50, 50])
+    elif trspawn == 10:
+        trap = 'i'
+        rstart != 'i'
+        pygame.draw.rect(screen, RED, [795, 498, 50, 50])
+    elif trspawn == 11:
+        trap = 'd'
+        rstart != 'd'
+        pygame.draw.rect(screen, RED, [695, 378, 50, 50])
+    
 spawn()
